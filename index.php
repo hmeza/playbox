d3f86c798eb5a29b07a5cefec17179fdf350219b
Hugo Meza <hugo.meza.macias@gmail.com>
2013-01-16 00:23:30 +0100

    Added draw html head method to view class. Fixed link to folder in music list.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once('lib/view.php');
 include 'conf/lang/esp.php';
 
 define('DEFAULT_LINES_SEPARATOR', '<br><br>');
-define('URL', 'http://myurl.com');
+define('URL', 'http://xxxxxxxxxxx.xxx');
 
 /**
  * Sort paths starting with folders.
@@ -18,100 +18,34 @@ function sortPaths($a, $b) {
     return (!$a->is_dir && $b->is_dir) ? 1 : 0;
 }
 
-function drawMusicList(dropbox $o_dropbox) {
-	$s_content = LANG_MY_PLAYLIST.'<br>';
-	$st_list = $o_dropbox->getMusicList();
-	foreach($st_list as $s_key => $s_entry) {
-		$s_content .= '<a href="index.php?path='.$s_key.'&remove=true">'.LANG_REMOVE.'</a> ';
-		$s_content .= '<a href="index.php?path='.$s_key.'&play=true">'.LANG_PLAY.'</a> ';
-		$s_content .= '<a href="'.$s_key.'">'.\dropbox::getNameFromPath($s_key).'</a><br>';
-	}
-	return $s_content;
-}
-
-function drawPlaylist($st_playlist) {
-	$s_content = '<div id="playlist">';
-	foreach($st_playlist as $st_entry) {
-		$s_content .= '<div href="'.$st_entry['url'].'" style="width: 400px;" class="item">
-			<div>
-			<div class="fr duration"></div>
-			<div class="btn play"></div>
-			<div class="title">'.$st_entry['path'].'</div>
-			</div>
-			<div class="player inactive"></div>
-			</div>';
-	}
-	$s_content .= '
-	</div>	
-	<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'prev\');">'.LANG_PREV.'</a>
-	<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'next\');">'.LANG_NEXT.'</a>
-	<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'pause\');"><img src="lib/drplayer/i/pause.gif" alt="'.LANG_PAUSE.'" title="'.LANG_PAUSE.'"></a>
-	<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'play\');"><img src="lib/drplayer/i/play.gif" alt="'.LANG_PLAY.'" title="'.LANG_PLAY.'"></a>';
-	return $s_content;
-}
-
-function drawFolderList($metaData, $s_path, $s_lastPath) {
-	$s_content = LANG_CURRENT_PATH.$s_path.'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-	$s_content .= '<a href="index.php?path='.$s_path.'&store=true">'.LANG_STORE_THIS_PATH.'</a><br>';
-	$s_content .= '<a href="index.php?path='.$s_lastPath.'">'.LANG_GO_BACK.'</a>'.DEFAULT_LINES_SEPARATOR;
-        usort($metaData['body']->contents, "sortPaths");
-	foreach($metaData['body']->contents as $o_item) {
-		if($o_item->is_dir == 1) {
-			$s_content .= '<a href="index.php?path='.$o_item->path.'">'.$o_item->path.'</a><br>';
-		}
-		else {
-			$s_content .= $o_item->path.'<br>';
-		}
-	}
-	return $s_content;
-}
-
 global $dropbox;
 $o_dropbox = new dropbox($dropbox);
 if(isset($_GET['path']) && !empty($_GET['path'])) $s_path = urldecode($_GET['path']);
 else $s_path = "/";
 $s_message =  '';
 $s_playlist = '';
-if(isset($_GET['store']) && $_GET['store'] == true) {
+if(isset($_GET['store'])) {
 	$st_list = $o_dropbox->getMusicList();
 	$o_dropbox->addMusicPath($s_path);
 	$o_dropbox->storeMusicList();
 	$s_message = LANG_STORE_SUCCESSFULLY;
 }
-if(isset($_GET['play']) && $_GET['play'] == true) {
-	$s_playlist = drawPlaylist($o_dropbox->getSharedPlaylist($_GET['path']));
+if(isset($_GET['play'])) {
+	$s_playlist = \view::drawPlaylist($o_dropbox->getSharedPlaylist($_GET['path']));
 	$s_message = LANG_NOW_PLAYING.dropbox::getNameFromPath($_GET['path']);
 }
-if(isset($_GET['remove']) && $_GET['remove'] == true) {
+if(isset($_GET['remove'])) {
 	$o_dropbox->removeMusicPath($_GET['path']);
 	$o_dropbox->storeMusicList();
 	$s_path = "/";
 }
 
-
-// Get html content to show folders and files
-$s_content = drawFolderList($dropbox->metaData($s_path), $s_path, \dropbox::getParentPath($s_path));
-// Get playlists
-$s_playlists = drawMusicList($o_dropbox);
-
 ?>
+<!-- Dirty, but in progress -->
 <html>
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="Stylesheet" href="lib/drplayer/drplayer.css" type="text/css" />
-
-    <script src="lib/js/jquery.js" type="text/javascript"></script>
-
-    <script src="lib/drplayer/drplayer.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $("#playlist").playlist(
-                {
-                    playerurl: "lib/drplayer/swf/drplayer.swf"
-                }
-            );
-        });
-    </script>
+<?php
+echo \view::head();
+?>
 <body>
 <?php
 echo \view::facebookLikeScript();
@@ -129,12 +63,12 @@ echo \view::googlePlusOne();
 <tr>
 <td width="50%" valign="top">
 <?php
-echo $s_content;
+echo \view::drawFolderList($dropbox->metaData($s_path), $s_path, \dropbox::getParentPath($s_path));
 ?>
 </td>
 <td valign="top">
 <?php
-echo $s_playlists.DEFAULT_LINES_SEPARATOR;
+echo \view::drawMusicList($o_dropbox).DEFAULT_LINES_SEPARATOR;
 echo $s_playlist.DEFAULT_LINES_SEPARATOR;
 ?>
 </td>
--- lib/view.php
+++ lib/view.php
@@ -1,6 +1,26 @@
 <?php
 
 class view {
+	static public function head() {
+		$s_return = '
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="Stylesheet" href="lib/drplayer/drplayer.css" type="text/css" />
+    <script src="lib/js/jquery.js" type="text/javascript"></script>
+    <script src="lib/drplayer/drplayer.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#playlist").playlist(
+                {
+                    playerurl: "lib/drplayer/swf/drplayer.swf"
+                }
+            );
+        });
+    </script>
+</head>';
+		return $s_return;
+	}
+	
 	static public function facebookLikeScript() {
 		$s_return = '<div id="fb-root"></div>
 <script>(function(d, s, id) {
@@ -33,10 +53,55 @@ class view {
 		return $s_return;
 	}
 	
+	static function drawMusicList(dropbox $o_dropbox) {
+		$s_content = LANG_MY_PLAYLIST.'<br>';
+		$st_list = $o_dropbox->getMusicList();
+		foreach($st_list as $s_key => $s_entry) {
+			$s_content .= '<a href="index.php?path='.$s_key.'&remove=true">'.LANG_REMOVE.'</a> ';
+			$s_content .= '<a href="index.php?path='.$s_key.'&play=true">'.LANG_PLAY.'</a> ';
+			$s_content .= '<a href="index.php?path='.$s_key.'">'.\dropbox::getNameFromPath($s_key).'</a><br>';
+		}
+		return $s_content;
+	}
 	
+	static function drawPlaylist($st_playlist) {
+		$s_content = '<div id="playlist">';
+		foreach($st_playlist as $st_entry) {
+			$s_content .= '<div href="'.$st_entry['url'].'" style="width: 400px;" class="item">
+				<div>
+				<div class="fr duration"></div>
+				<div class="btn play"></div>
+				<div class="title">'.$st_entry['path'].'</div>
+				</div>
+				<div class="player inactive"></div>
+				</div>';
+		}
+		$s_content .= '
+		</div>	
+		<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'prev\');">'.LANG_PREV.'</a>
+		<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'next\');">'.LANG_NEXT.'</a>
+		<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'pause\');"><img src="lib/drplayer/i/pause.gif" alt="'.LANG_PAUSE.'" title="'.LANG_PAUSE.'"></a>
+		<a href="javascript:void(0);" onClick="$(\'#playlist\').playlist(\'play\');"><img src="lib/drplayer/i/play.gif" alt="'.LANG_PLAY.'" title="'.LANG_PLAY.'"></a>';
+		return $s_content;
+	}
+	
+	static function drawFolderList($metaData, $s_path, $s_lastPath) {
+		$s_content = LANG_CURRENT_PATH.$s_path.'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+		$s_content .= '<a href="index.php?path='.$s_path.'&store=true">'.LANG_STORE_THIS_PATH.'</a><br>';
+		$s_content .= '<a href="index.php?path='.$s_lastPath.'">'.LANG_GO_BACK.'</a>'.DEFAULT_LINES_SEPARATOR;
+	        usort($metaData['body']->contents, "sortPaths");
+		foreach($metaData['body']->contents as $o_item) {
+			if($o_item->is_dir == 1) {
+				$s_content .= '<a href="index.php?path='.$o_item->path.'">'.$o_item->path.'</a><br>';
+			}
+			else {
+				$s_content .= $o_item->path.'<br>';
+			}
+		}
+		return $s_content;
+	}
 	
 	static public function main() {
-		
 	}
 }
 ?>
