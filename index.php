a707bddb9900df24dcec6c3f9cc71c0327690cb6
Hugo Meza <hugo.meza.macias@gmail.com>
2013-03-25 23:45:53 +0100

    Moved HTML lines separator to conf file.

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ require_once('lib/view.php');
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 include 'conf/lang/'.$lang.'.php';
 
-define('DEFAULT_LINES_SEPARATOR', '<br><br>');
-
 /**
  * Sort paths starting with folders.
  * @param Object $a
